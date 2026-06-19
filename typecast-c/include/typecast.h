84abdd1b7dfd4c3db96093f94fda573a8d93a08c
hmmhmmhm/ <hamin@neosapience.com>
2026-06-19 16:32:08 +0900

    chore: bump SDK versions for target_lufs release (#52)

--- typecast-c/include/typecast.h
+++ typecast-c/include/typecast.h
@@ -74,8 +74,8 @@ extern "C" {
 /* Library version */
 #define TYPECAST_VERSION_MAJOR 1
 #define TYPECAST_VERSION_MINOR 2
-#define TYPECAST_VERSION_PATCH 4
-#define TYPECAST_VERSION "1.2.4"
+#define TYPECAST_VERSION_PATCH 5
+#define TYPECAST_VERSION "1.2.5"
 
 /*
  * DLL Export/Import macros for Windows
